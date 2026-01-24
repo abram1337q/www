3a3d106d2e63513f3b6a1670011fba3a4f24d069
abram1337q <ip.dev7348@gmail.com>
2026-01-24 17:19:55 +0700

    Update scroll-area.tsx

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -15,7 +15,7 @@ function ScrollArea({
       className={cn("relative overflow-hidden", className)}
       {...props}
     >
-      <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
+      <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit] [&>div]:!block">
         {children}
       </ScrollAreaPrimitive.Viewport>
       <ScrollBar />
@@ -34,16 +34,22 @@ function ScrollBar({
       data-slot="scroll-bar"
       orientation={orientation}
       className={cn(
-        "flex touch-none select-none transition-colors",
+        "flex touch-none select-none transition-all duration-200",
         orientation === "vertical" &&
-          "h-full w-2.5 border-l border-l-transparent p-[1px]",
+          "h-full w-2 hover:w-2.5 border-l border-l-transparent p-px",
         orientation === "horizontal" &&
-          "h-2.5 flex-col border-t border-t-transparent p-[1px]",
+          "h-2 hover:h-2.5 flex-col border-t border-t-transparent p-px",
         className
       )}
       {...props}
     >
-      <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-zinc-200 dark:bg-zinc-800" />
+      <ScrollAreaPrimitive.ScrollAreaThumb
+        className={cn(
+          "relative flex-1 rounded-full",
+          "bg-zinc-200 hover:bg-zinc-300 transition-colors duration-200",
+          "dark:bg-zinc-700 dark:hover:bg-zinc-600"
+        )}
+      />
     </ScrollAreaPrimitive.ScrollAreaScrollbar>
   )
 }
